446bd76b898558b505f8a530ef32d8531b9e95c3
MaRuF2131 <marufahmmed916@gmail.com>
2026-04-20 20:06:54 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
 
+import Image from "next/image";
+import EducationAndSkill from "@/app/components/educationAndExperience";
+import Skill from "@/app/components/skills";
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1>Home</h1>    
+      <EducationAndSkill></EducationAndSkill>
+      <Skill></Skill>
     </>
   );
 }
